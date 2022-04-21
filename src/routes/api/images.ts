61fe1fe8c12dc1040e69f0f71ce74f79498d29bd
Mostafa Ghonem <mostafaghonem90@yahoo.com>
2022-04-21 19:59:05 +0200

    Add files via upload

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -8,33 +8,32 @@ const currentPath = __dirname;
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    // try {
+    try {
+        let imagName: string = req.query.filename as string;
+        let width = parseInt(req.query.width as unknown as string);
+        let height = parseInt(req.query.height as unknown as string);
 
-    // } catch (err) {
-    //     res.status(400).send('check the image Name');
-    // }
-    let imagName: string = req.query.filename as string;
-    let width = parseInt((req.query.width as unknown)as string ) ;
-    let height = parseInt((req.query.height as unknown) as string);
-    
-    let imgPath = path
-        .join(currentPath, 'images', imagName)
-        .replace(/\\/g, '/');
-    imgPath += '.jpg';
+        let imgPath = path
+            .join(currentPath, 'images', imagName)
+            .replace(/\\/g, '/');
+        imgPath += '.jpg';
 
-    await fs.readFile(imgPath); //if image name is incorect , it will throw an error
-    //else
-    await sharp(imgPath)
-        .resize({
-            width: width,
-            height: height,
-        })
-        .toFile(__dirname + `/thumb/${imagName}.jpg`);
-    let resizedImg = path
-        .join(currentPath, 'thumb', imagName)
-        .replace(/\\/g, '/');
-    resizedImg += '.jpg';
-    res.status(200).sendFile(resizedImg);
+        await fs.readFile(imgPath); //if image name is incorect , it will throw an error
+        //else
+        await sharp(imgPath)
+            .resize({
+                width: width,
+                height: height,
+            })
+            .toFile(__dirname + `/thumb/${imagName}.jpg`);
+        let resizedImg = path
+            .join(currentPath, 'thumb', imagName)
+            .replace(/\\/g, '/');
+        resizedImg += '.jpg';
+        res.status(200).sendFile(resizedImg);
+    } catch (err) {
+        res.status(400).send('check the image Name');
+    }
 });
 
 export default router;
