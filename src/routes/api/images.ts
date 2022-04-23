a9d5c85b2b32e00603c47f1f0f5eb1bf26ea79de
mostafaghonem <mostafaghonem90@yahoo.com>
2022-04-23 19:35:04 +0200

    modify

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import resizedImg from '../../util/resizeImg';
 import imgExist from '../../util/imgExist';
@@ -24,44 +23,3 @@ router.get('/', async (req, res) => {
 });
 
 export default router;
-=======
-import express from 'express';
-import { promises as fs } from 'fs';
-import path from 'path';
-const sharp = require('sharp');
-
-const currentPath = __dirname;
-
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    try {
-        let imagName: string = req.query.filename as string;
-        let width = parseInt(req.query.width as unknown as string);
-        let height = parseInt(req.query.height as unknown as string);
-
-        let imgPath = path
-            .join(currentPath, 'images', imagName)
-            .replace(/\\/g, '/');
-        imgPath += '.jpg';
-
-        await fs.readFile(imgPath); //if image name is incorect , it will throw an error
-        //else
-        await sharp(imgPath)
-            .resize({
-                width: width,
-                height: height,
-            })
-            .toFile(__dirname + `/thumb/${imagName}.jpg`);
-        let resizedImg = path
-            .join(currentPath, 'thumb', imagName)
-            .replace(/\\/g, '/');
-        resizedImg += '.jpg';
-        res.status(200).sendFile(resizedImg);
-    } catch (err) {
-        res.status(400).send('check the image Name');
-    }
-});
-
-export default router;
->>>>>>> 5a13ea8290813a89db280eccf98001c339696b03
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import images from './api/images';
 
@@ -12,19 +11,4 @@ router.use('/images' , images);
 
 
 
-=======
-import express from "express";
-import images from './api/images';
-
-const router = express.Router();
-
-
-router.get('/' , (req , res)=>{
-    res.send('Welcome to the API');
-})
-router.use('/images' , images);
-
-
-
->>>>>>> 5a13ea8290813a89db280eccf98001c339696b03
 export default router;
