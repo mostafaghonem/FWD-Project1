f35959ad215785962730bbd659c2327e2c078ae9
mostafaghonem <mostafaghonem90@yahoo.com>
2022-04-23 19:51:46 +0200

    modify

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {DisplayProcessor, SpecReporter, StacktraceOption} from "jasmine-spec-reporter";
 import SuiteInfo = jasmine.SuiteInfo;
 
@@ -14,21 +13,4 @@ jasmine.getEnv().addReporter(new SpecReporter({
         displayStacktrace: StacktraceOption.NONE
     },
     customProcessors: [CustomProcessor],
-=======
-import {DisplayProcessor, SpecReporter, StacktraceOption} from "jasmine-spec-reporter";
-import SuiteInfo = jasmine.SuiteInfo;
-
-class CustomProcessor extends DisplayProcessor {
-    public displayJasmineStarted(info: SuiteInfo, log: string): string {
-        return `${log}`;
-    }
-}
-
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new SpecReporter({
-    spec: {
-        displayStacktrace: StacktraceOption.NONE
-    },
-    customProcessors: [CustomProcessor],
->>>>>>> 5a13ea8290813a89db280eccf98001c339696b03
 }));
