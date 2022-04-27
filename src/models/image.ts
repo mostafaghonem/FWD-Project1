aaa8803af3acd87ee0bdaec3551712674957ca93
mostafaghonem <mostafaghonem90@yahoo.com>
2022-04-27 16:30:57 +0200

    modify input validation

--- src/models/image.ts
+++ src/models/image.ts
@@ -4,8 +4,8 @@ import Joi, { func, string } from 'joi';
 function validateEndpoint(img:object){
     let schema = Joi.object({
         name :Joi.string().required(),
-        width : Joi.number().required(),
-        height : Joi.number().required()
+        width : Joi.number().required().not(0).positive(),
+        height : Joi.number().required().not(0).positive()
     })
     return schema.validate(img);
 }
